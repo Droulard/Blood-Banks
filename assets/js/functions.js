00fca56e5a972da8e9d3365155028a105b7cf1ff
Kyle <Kyle.Droulard@gmail.com>
2019-04-09 11:53:36 -0400

    Update Web Side

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,19 +1,3 @@
-function CheckInventoryTransForm()
-{
-	console.log("inventory transactions")
-	var f = document.forms['InventoryTransForm'].elements;
-	var cansubmit = true;
-	
-	for (var i = 0; i < f.length; i++) {
-		if (f[i].value.length == 0) { 
-			cansubmit = false;
-			break;
-		}
-	}
-	
-	document.getElementById('InventoryTransSubmitButton').disabled = !cansubmit;
-}
-
 function CheckRequestForm()
 {
 	console.log("request")
